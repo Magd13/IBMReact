e6307c1eb30b40db5053f62ba31f3823249c497f
Magd13 <joelmagdielriveraarce@gmail.com>
2025-05-06 21:11:49 -0400

    Confiration mongodb

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:fMEqxZiBEPvFJDYsF5PMYmD7@172.21.99.103:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
